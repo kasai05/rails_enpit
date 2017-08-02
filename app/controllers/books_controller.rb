33b0efefdfd20ba281b72ae7eab9dec5c909313b
Douzono <douzono.koji@gmail.com>
2017-08-02 19:42:33 +0900

    修正。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,12 @@ class BooksController < ApplicationController
   # GET /books.json
   def index
     @books = Book.all
- 
-  #map display  
-    url =  "http://ip-api.com/json/"
-    res = urlopen(url).read().decode('utf-8')
-    @lat = json.loads(res)['lat']
-    @lon = json.loads(res)['lon']
-    @city = json.loads(res)['city']
+   #map display  
+   # url =  "http://ip-api.com/json/"
+   # res = urlopen(url).read().decode('utf-8')
+   # @lat = json.loads(res)['lat']
+   # @lon = json.loads(res)['lon']
+   # @city = json.loads(res)['city']
   end
 
   # GET /books/1
